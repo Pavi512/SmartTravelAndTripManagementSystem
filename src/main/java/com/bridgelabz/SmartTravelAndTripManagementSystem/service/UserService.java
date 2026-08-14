2be04dd6b6863b6da1bf9815e8da2a09bda6962c
Pavi512 <gondipavithra@gmail.com>
2026-08-15 01:12:57 +0530

    [Pavithra] : Added user service interface

---
+++ src/main/java/com/bridgelabz/SmartTravelAndTripManagementSystem/service/UserService.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.SmartTravelAndTripManagementSystem.service;
+
+import com.bridgelabz.SmartTravelAndTripManagementSystem.dto.UserRequestDTO;
+import com.bridgelabz.SmartTravelAndTripManagementSystem.dto.UserResponseDTO;
+
+import java.util.List;
+
+// Service interface
+public interface UserService {
+
+    // Registers a new user
+    public UserResponseDTO registerUser(UserRequestDTO user);
+
+    // Retrieves all users
+    public List<UserResponseDTO> getAllUsers();
+
+    // Retrieves a user using ID
+    public UserResponseDTO getUserById(long id);
+
+    // Updates an existing user using ID
+    public UserResponseDTO updateUser(long id, UserRequestDTO user);
+}
