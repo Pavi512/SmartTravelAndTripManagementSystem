c287619cebc73f16118ec1f30047925ef43ba293
Pavi512 <gondipavithra@gmail.com>
2026-08-15 01:18:02 +0530

    [Pavithra] : Added Spring Boot application test

---
+++ src/test/java/com/bridgelabz/SmartTravelAndTripManagementSystem/SmartTravelAndTripManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.SmartTravelAndTripManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTravelAndTripManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
