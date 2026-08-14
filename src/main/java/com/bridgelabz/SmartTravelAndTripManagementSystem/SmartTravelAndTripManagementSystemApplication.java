215b22dc5a8901dabc1148722fe292a4c87f88d1
Pavi512 <gondipavithra@gmail.com>
2026-08-15 01:17:55 +0530

    [Pavithra] : Added Spring Boot application configuration

---
+++ src/main/java/com/bridgelabz/SmartTravelAndTripManagementSystem/SmartTravelAndTripManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.SmartTravelAndTripManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTravelAndTripManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTravelAndTripManagementSystemApplication.class, args);
+	}
+
+}
