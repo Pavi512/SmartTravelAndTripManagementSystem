3e70f865c39d4618c18b73d5eade9f6d04c3a9e9
PalagiriNagayogeswari <yogi34343@gmail.com>
2026-08-15 16:27:59 +0530

    [Nagayogeswari:Committed Destination Service]

---
+++ src/main/java/com/bridgelabz/SmartTravelAndTripManagementSystem/service/DestinationService.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.SmartTravelAndTripManagementSystem.service;
+
+import com.bridgelabz.SmartTravelAndTripManagementSystem.dto.TravelPackageResponse;
+import com.bridgelabz.SmartTravelAndTripManagementSystem.dto.TravelPackageRequest;
+import com.bridgelabz.SmartTravelAndTripManagementSystem.dto.DestinationResponse;
+import com.bridgelabz.SmartTravelAndTripManagementSystem.dto.DestinationRequest;
+
+
+import java.util.List;
+
+public interface DestinationService {
+
+    DestinationResponse createDestination(
+            DestinationRequest request);
+
+    List<DestinationResponse> getAllDestinations();
+
+    DestinationResponse getDestinationById(Long id);
+
+    TravelPackageResponse addTravelPackage(
+            Long destinationId,
+            TravelPackageRequest request);
+}
