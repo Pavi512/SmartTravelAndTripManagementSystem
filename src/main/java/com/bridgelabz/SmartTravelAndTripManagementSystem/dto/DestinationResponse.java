37a5b1f90cd7173d313f0c64057495eead0afa14
PalagiriNagayogeswari <yogi34343@gmail.com>
2026-08-15 16:21:13 +0530

    [Nagayogeswari:Committed Destination Response DTO]

---
+++ src/main/java/com/bridgelabz/SmartTravelAndTripManagementSystem/dto/DestinationResponse.java
@@ -0,0 +1,56 @@
+package com.bridgelabz.SmartTravelAndTripManagementSystem.dto;
+
+public class DestinationResponse {
+
+    private Long id;
+    private String name;
+    private String country;
+    private String description;
+
+    public DestinationResponse() {
+    }
+
+    public DestinationResponse(
+            Long id,
+            String name,
+            String country,
+            String description) {
+
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
